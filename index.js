ba39d2d3919407254ed100d44595ab44f2151880
moath62how <moath26how@gmail.com>
2024-12-22 02:01:55 +0200

    Implement smooth scrolling and active link highlighting in navigation

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
-const UpBtn = document.querySelector('#upButton');
-
-if (UpBtn) {
-    window.addEventListener('scroll', () => {
-        console.log(window.scrollY);
-
-        if (window.scrollY >= 500) {
-            UpBtn.classList.remove('hidden');
-        } else {
-            UpBtn.classList.add('hidden');
-        }
-    })
-
-    UpBtn.addEventListener('click', () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        })
-    })
-}
-
 const form = document.querySelector('.login-form');
 const submitBtn = document.querySelector('#submitBtn');
 
 if (form && submitBtn) {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        if (form.checkValidity()) {
-            window.location.href = './index.html';
-        } else {
-            form.reportValidity();
-        }
-    });
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (form.checkValidity()) {
+      window.location.href = './index.html';
+    } else {
+      form.reportValidity();
+    }
+  });
 }
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
+
+console.log('Test');
+
+const s = document.querySelector('main');
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('nav>ul>li>a');
+navLinks[0].classList.add('active');
+s.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - s.offsetTop;
+    const sectionHeight = section.clientHeight;
+    console.log(sectionTop, sectionHeight);
+
+    if (s.scrollTop >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  console.log('Current section:', current);
+
+  navLinks.forEach((link) => {
+    link.classList.remove('active');
+    console.log(link.getAttribute('href'), current);
+
+    if (link.getAttribute('href').includes(current)) {
+      link.classList.add('active');
+    }
+  });
+});
